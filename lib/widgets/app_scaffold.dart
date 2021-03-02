09e494d838097f76633e929613fedf9ac0c3bcde
Gary <gary.ong@utopia.my>
2021-03-02 13:35:54 +0800

    ✨ Added reusable widgets, AppText and AppScaffold

---
+++ lib/widgets/app_scaffold.dart
@@ -0,0 +1,81 @@
+import 'package:expense_tracker/themes/styles.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+
+class AppScaffold extends StatelessWidget {
+  final bool appBar;
+  final Widget titleChild;
+  final Color backgroundColor;
+  final Color appBarBackgroundColor;
+  final Color backButtonColor;
+  final Widget child;
+  final Key scaffoldKey;
+  final Widget drawer;
+  final Widget tabBar;
+  final bool isElevation;
+  final bool isBackButton;
+  final bool isDrawer;
+  final bool isActionBar;
+  final bool isScrollView;
+  final Widget floatingActionButton;
+  final Widget actionBar;
+  final Widget bottomNavigatorBar;
+  final Widget leadingIcon;
+
+  const AppScaffold({
+    this.titleChild,
+    this.backgroundColor: Colors.white,
+    this.child,
+    this.appBarBackgroundColor: const Color(0xff00adef),
+    this.backButtonColor: Colors.white,
+    this.isElevation: false,
+    this.isBackButton: true,
+    this.appBar: true,
+    this.isDrawer: true,
+    this.isScrollView: true,
+    this.scaffoldKey,
+    this.drawer,
+    this.tabBar,
+    this.isActionBar,
+    this.floatingActionButton,
+    this.actionBar,
+    this.bottomNavigatorBar,
+    this.leadingIcon,
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).requestFocus(FocusNode());
+        SystemChannels.textInput.invokeMethod('TextInput.hide');
+      },
+      child: Scaffold(
+        key: scaffoldKey,
+        backgroundColor: backgroundColor,
+        bottomNavigationBar: bottomNavigatorBar ?? null,
+        appBar: appBar
+            ? AppBar(
+                leading: isBackButton ? leadingIcon : Container(),
+                backgroundColor: appBarBackgroundColor,
+                elevation: isElevation ? AppBarElevation.normal : 0,
+                title: Center(
+                  child: titleChild,
+                ),
+                iconTheme: IconThemeData(color: backButtonColor),
+                actions: <Widget>[],
+                bottom: tabBar,
+              )
+            : null,
+        floatingActionButton: floatingActionButton,
+        body: isScrollView
+            ? SingleChildScrollView(
+                child: child,
+              )
+            : child,
+      ),
+    );
+  }
+}
---
+++ lib/widgets/app_text.dart
@@ -0,0 +1,52 @@
+import 'package:expense_tracker/themes/styles.dart';
+import 'package:flutter/material.dart';
+
+class AppText extends StatelessWidget {
+  final String text;
+  final String fontFamily;
+  final double fontSize;
+  final FontWeight fontWeight;
+  final Color color;
+  final TextAlign textAlign;
+  final TextOverflow overflow;
+  final int maxlines;
+  final FontStyle fontStype;
+  final bool underline;
+  final bool isStrikeThrough;
+  final bool isTextShadow;
+  final bool isTranslate;
+
+  const AppText(this.text,
+      {this.fontFamily,
+      this.fontSize,
+      this.fontWeight,
+      this.color: Colors.black,
+      this.fontStype,
+      this.textAlign,
+      this.overflow: TextOverflow.ellipsis,
+      this.maxlines: 3,
+      this.underline: false,
+      this.isStrikeThrough: false,
+      this.isTextShadow: false,
+      this.isTranslate: true});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      overflow: overflow,
+      maxLines: maxlines,
+      style: TextStyle(
+        color: color,
+        fontSize: fontSize ?? AppFontSize.medium,
+        fontWeight: fontWeight,
+        fontStyle: fontStype,
+        fontFamily: fontFamily,
+        decoration: underline
+            ? TextDecoration.underline
+            : isStrikeThrough ? TextDecoration.lineThrough : null,
+        shadows: isTextShadow ? AppTextShadow.normal : null,
+      ),
+    );
+  }
+}
